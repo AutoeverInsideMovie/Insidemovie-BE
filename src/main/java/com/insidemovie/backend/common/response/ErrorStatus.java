06c80f3c1f1e6435b2b910ae4cd07d981d73e0f5
Gumraze-git <galaxydh4110@gmail.com>
2025-07-14 19:16:51 +0900

    [FEAT] ErrorStatus에 EXTERNAL_SERVICE_ERROR 추가
    - 외부 감정 분석 서비스 호출 실패 상황에 대한 상태 코드 및 메시지 정의 추가

--- src/main/java/com/insidemovie/backend/common/response/ErrorStatus.java
+++ src/main/java/com/insidemovie/backend/common/response/ErrorStatus.java
@@ -31,6 +31,8 @@ public enum ErrorStatus {
     /** 500 SERVER_ERROR */
     FAIL_UPLOAD_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR,"파일 업로드 실패하였습니다."),
 
+    /** 503 ERROR */
+    EXTERNAL_SERVICE_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "외부 감정 분석 서비스 호출에 실패하였습니다.")
     ;
 
     private final HttpStatus httpStatus;

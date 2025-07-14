3fb1d03b949191e2bbd06448fd44249e1a8e8a37
Gumraze-git <galaxydh4110@gmail.com>
2025-07-14 19:48:50 +0900

    [FEAT] EmotionRepository에 findByReviewId 메서드 추가
    - 리뷰 ID를 기반으로 Emotion 엔티티를 조회하기 위한 메서드 추가

--- src/main/java/com/insidemovie/backend/api/review/repository/EmotionRespository.java
+++ src/main/java/com/insidemovie/backend/api/review/repository/EmotionRespository.java
@@ -2,6 +2,8 @@ package com.insidemovie.backend.api.review.repository;
 
 import com.insidemovie.backend.api.review.entity.Emotion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface EmotionRespository extends JpaRepository<Emotion, Long> {
+    Optional<Emotion> findByReviewId(Long reviewId);
 }

08ac2ae199f19c8b29110caafd4bc005639a80dd
yangjiseonn <yangjiseonn@gmail.com>
2025-07-16 18:15:27 +0900

    [FIX] 사용자, 리뷰, 신고 엔티티 수정

--- src/main/java/com/insidemovie/backend/api/review/entity/Review.java
+++ src/main/java/com/insidemovie/backend/api/review/entity/Review.java
@@ -2,6 +2,7 @@ package com.insidemovie.backend.api.review.entity;
 
 import com.insidemovie.backend.api.member.entity.Member;
 import com.insidemovie.backend.api.movie.entity.Movie;
+import com.insidemovie.backend.api.report.entity.Report;
 import com.insidemovie.backend.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -37,6 +38,12 @@ public class Review extends BaseTimeEntity {
     private LocalDateTime watchedAt;
     private boolean spoiler;
 
+    @Column(name = "is_reported", nullable = false)
+    private boolean isReported = false;  // 신고여부
+
+    @Column(name = "is_concealed", nullable = false)
+    private boolean isConcealed = false;  // 삭제여부
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
@@ -51,12 +58,25 @@ public class Review extends BaseTimeEntity {
     @OneToMany(mappedBy = "review", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ReviewLike> likes = new ArrayList<>();
 
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Report> reports = new ArrayList<>();
+
     public void modify(String content, double rating, boolean spoiler, LocalDateTime watchedAt) {
         this.content = content;
         this.rating = rating;
         this.spoiler = spoiler;
         this.watchedAt = watchedAt;
         this.modify = true;  // 수정됨
+    }
+
+    // 신고가 등록되면 호출
+    public void markReported() {
+        this.isReported = true;
+    }
+
+    // 관리자 수용 시 호출
+    public void markConcealed() {
+        this.isConcealed = true;
     }
 
 }

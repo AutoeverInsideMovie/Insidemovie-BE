d3351afb297f116645161626406423eab5ae12f6
yangjiseonn <yangjiseonn@gmail.com>
2025-07-16 21:07:13 +0900

    [FIX] 리뷰 신고 기능 수정

--- src/main/java/com/insidemovie/backend/api/report/repository/ReportRepository.java
+++ src/main/java/com/insidemovie/backend/api/report/repository/ReportRepository.java
@@ -3,8 +3,6 @@ package com.insidemovie.backend.api.report.repository;
 import com.insidemovie.backend.api.member.entity.Member;
 import com.insidemovie.backend.api.report.entity.Report;
 import com.insidemovie.backend.api.review.entity.Review;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -13,7 +11,4 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
 
     // 동일 사용자가 동일 리뷰를 이미 신고했는지 확인
     boolean existsByReviewAndReporter(Review review, Member reporter);
-
-    // 관리자용 전체 신고 페이징 조회
-    Page<Report> findAll(Pageable pageable);
 }

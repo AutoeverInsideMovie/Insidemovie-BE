33cdf233aea07075ad2c803539030ecd89ea1b41
Gumraze-git <galaxydh4110@gmail.com>
2025-07-17 17:36:57 +0900

    [FEAT] TmdbMovieDto 필드 추가 및 주석 정리
    - `voteCount`, `popularity` 필드 추가
    - 필드 정의 형식 통일화를 위한 주석 위치 및 정렬 수정

--- src/main/java/com/insidemovie/backend/api/movie/dto/tmdb/TmdbMovieDto.java
+++ src/main/java/com/insidemovie/backend/api/movie/dto/tmdb/TmdbMovieDto.java
@@ -17,21 +17,27 @@ public class TmdbMovieDto {
     private String overview; //줄거리
 
     @JsonProperty("poster_path")
-    private String posterPath; //포스터
+    private String posterPath;          // 포스터
 
     @JsonProperty("backdrop_path")
-    private String backDropPath; //배경 이미지
+    private String backDropPath;        // 배경 이미지
 
     @JsonProperty("vote_average")
-    private Double voteAverage; //평균 평점
+    private Double voteAverage;         // 평균 평점
+
+    @JsonProperty("vote_count")
+    private Integer voteCount;
 
     @JsonProperty("release_date")
-    private LocalDate releaseDate; //개봉일
+    private LocalDate releaseDate;      // 개봉일
 
     @JsonProperty("genre_ids")
-    private List<Integer> genreIds; //장르
+    private List<Integer> genreIds;     //장르
 
     @JsonProperty("original_language")
-    private String originalLanguage; //국가
+    private String originalLanguage;    // 국가
+
+    @JsonProperty("popularity")
+    private Double popularity;          // 인기
 
 }

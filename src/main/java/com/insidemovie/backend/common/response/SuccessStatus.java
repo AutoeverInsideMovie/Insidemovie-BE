b4eaef26d8af013fc5ac68f663572a01ec41a8e1
Gumraze-git <galaxydh4110@gmail.com>
2025-07-18 12:38:38 +0900

    [FEAT] SuccessStatus에 감정 상태 업데이트 응답 코드 추가
    - `UPDATE_EMOTION_SUMMARY_SUCCESS` 상태 및 메시지 추가

--- src/main/java/com/insidemovie/backend/common/response/SuccessStatus.java
+++ src/main/java/com/insidemovie/backend/common/response/SuccessStatus.java
@@ -38,6 +38,7 @@ public enum SuccessStatus {
     SEND_EMOTION_SUMMARY_SUCCESS(HttpStatus.OK, "내 감정 평균 조회 성공"),
     SEND_MOVIE_EMOTION_SUCCESS(HttpStatus.OK, "영화 감정 상태 조회 성공"),
     SEND_INITIAL_EMOTION_SUMMARY_SUCCESS(HttpStatus.OK, "초기 감정 상태 등록 완료"),
+    UPDATE_EMOTION_SUMMARY_SUCCESS(HttpStatus.OK, "사용자 감정 상태 업데이트 완료"),
 
 
     /** 201 CREATED */

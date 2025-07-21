f367493463ed998afe3db81f538cf078f4f33ac5
yangjiseonn <yangjiseonn@gmail.com>
2025-07-21 21:23:41 +0900

    [FEAT] 리뷰 응답에 사용자 대표 감정(memberEmotion) 추가

--- src/main/java/com/insidemovie/backend/api/member/repository/MemberEmotionSummaryRepository.java
+++ src/main/java/com/insidemovie/backend/api/member/repository/MemberEmotionSummaryRepository.java
@@ -3,6 +3,10 @@ package com.insidemovie.backend.api.member.repository;
 import com.insidemovie.backend.api.member.entity.MemberEmotionSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberEmotionSummaryRepository extends JpaRepository<MemberEmotionSummary, Long> {
     boolean existsByMemberId(Long memberId);
+
+    Optional<MemberEmotionSummary> findByMemberId(Long memberId);
 }

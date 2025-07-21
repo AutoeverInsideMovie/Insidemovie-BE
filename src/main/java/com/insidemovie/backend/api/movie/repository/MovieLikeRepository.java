e62e6e02b39a1dc272e58128e03042a51d648123
Gumraze-git <galaxydh4110@gmail.com>
2025-07-21 21:04:55 +0900

    [FEAT] MovieLikeRepository 메서드 추가 및 수정
    - 특정 영화 기반 좋아요 조회(findByMovie) 메서드 추가
    - 불필요한 import 및 공백 정리
    - findByMovie 메서드 시그니처 및 관련 코드 정리

--- src/main/java/com/insidemovie/backend/api/movie/repository/MovieLikeRepository.java
+++ src/main/java/com/insidemovie/backend/api/movie/repository/MovieLikeRepository.java
@@ -1,13 +1,12 @@
 package com.insidemovie.backend.api.movie.repository;
 
 import com.insidemovie.backend.api.member.entity.Member;
+import com.insidemovie.backend.api.movie.entity.Movie;
 import com.insidemovie.backend.api.movie.entity.MovieLike;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,11 +21,9 @@ public interface MovieLikeRepository extends JpaRepository<MovieLike, Long> {
     // 영화 + 유저 존재 여부 확인
     Boolean existsByMovie_IdAndMember_Id(Long movieId, Long memberId);
 
-
     // 좋아요 한 영화 개수 조회
     int countByMember_Id(Long memberId);
-
     Page<MovieLike> findByMovie(Member member, Pageable pageable);
-
     List<MovieLike> findByMember_Id(Long memberId);
+    List<MovieLike> findByMovie(Movie movie);
 }

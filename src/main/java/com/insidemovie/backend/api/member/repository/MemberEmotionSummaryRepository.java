c4704699babbc74a8e9865dbb3710d102253cb8b
Gumraze-git <galaxydh4110@gmail.com>
2025-07-21 21:04:50 +0900

    [FEAT] MemberEmotionSummaryRepository 메서드 추가 및 수정
    - 회원 ID 기반 감정 요약 조회(findByMemberId) 메서드 추가
    - Member 엔티티 기반 감정 요약 조회(findByMember) 메서드 추가
    - 회원 ID 기반 감정 요약 삭제(deleteByMemberId) 메서드 추가

--- src/main/java/com/insidemovie/backend/api/member/repository/MemberEmotionSummaryRepository.java
+++ src/main/java/com/insidemovie/backend/api/member/repository/MemberEmotionSummaryRepository.java
@@ -1,8 +1,16 @@
 package com.insidemovie.backend.api.member.repository;
 
+import com.insidemovie.backend.api.member.entity.Member;
 import com.insidemovie.backend.api.member.entity.MemberEmotionSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface MemberEmotionSummaryRepository extends JpaRepository<MemberEmotionSummary, Long> {
     boolean existsByMemberId(Long memberId);
+
+    void deleteByMemberId(Long memberId);
+
+    MemberEmotionSummary findByMemberId(Long memberId);
+
+    Optional<MemberEmotionSummary> findByMember(Member member);
 }
